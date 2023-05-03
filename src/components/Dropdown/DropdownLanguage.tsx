b77666deb5db6562da7f5c2ac576b1482be85dc7
Ahsan Abdullah <M.ahsan8588@gmail.com>
2023-05-03 10:24:35 +0500

    fix:font

--- src/components/Dropdown/DropdownLanguage.tsx
+++ src/components/Dropdown/DropdownLanguage.tsx
@@ -26,7 +26,7 @@ export default function DropdownLanguage() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right  bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
@@ -37,7 +37,7 @@ export default function DropdownLanguage() {
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                 English
+                English
                 </a>
               )}
             </Menu.Item>
@@ -50,7 +50,7 @@ export default function DropdownLanguage() {
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                  Urdu
+                  Chinese - 中文
                 </a>
               )}
             </Menu.Item>
@@ -63,7 +63,20 @@ export default function DropdownLanguage() {
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                  China
+                  German - Deutsch
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                 Italian - italiano
                 </a>
               )}
             </Menu.Item>
@@ -77,7 +90,7 @@ export default function DropdownLanguage() {
                       'block w-full px-4 py-2 text-left text-sm'
                     )}
                   >
-                    France
+                    French - français
                   </button>
                 )}
               </Menu.Item>
